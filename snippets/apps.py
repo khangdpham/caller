549877732d8fd10a266e12b52569df18cc386b8b
Khang Pham <Khang.pham@agilesi.com>
2022-06-01 16:44:58 -0700

    Write __init__ to get AppConfig running at start'

--- snippets/apps.py
+++ snippets/apps.py
@@ -5,9 +5,10 @@ class SnippetsConfig(AppConfig):
     name = 'snippets'
     def ready(self):
       from snippets.scheduleUpdater import updater
+      print("???Starting APPP")
       try:
         updater.start()
         print("Starting APPP")
       except Exception as e:
         print(str(e))
-~                       
+                      
